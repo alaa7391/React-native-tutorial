ca7655b889dba0d647a58f58596f06c1c46672b6
alaa.jaber <alaa.jaber@osolutions.tech>
2020-03-20 10:39:16 +0200

    react native start

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 
-const HomeScreen = ({ navigation }) => {
-  return (
-    <View>
-      <Text style={{ fontSize: 30 }}>HomeScreen</Text>
-      <Button title="Layout" onPress={() => navigation.navigate('Layout')} />
-    </View>
-  );
-};
-
-HomeScreen.navigationOptions = {
-  title: 'Home'
-};
+const HomeScreen = () => {
+  return  <Text style={styles.text}>Osolutions Batata App</Text>;
+    };
+ 
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
 
 export default HomeScreen;
